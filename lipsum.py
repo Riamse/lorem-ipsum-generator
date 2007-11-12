5633293348d71c72856772c9cd9c3832e34957c7
jhales.perth <jhales.perth@ea057288-b53e-0410-a93f-1b578e867aa5>
2007-11-12 06:47:35 +0000

    Initial commit.
    Program is in working order. GUI is fairly well-organised. The "lipsum" module could be cleaned up a bit.

---
+++ lipsum.py
@@ -0,0 +1,239 @@
+import math
+import random
+import string
+import os.path
+import re
+
+class generator:
+	"""
+	Generates random sentences or paragraphs of "lorem ipsum" text, 
+	based on the word distribution of a given sample text, and the 
+	words in a given dictionary.
+	"""
+
+	__punctuation_end_word = ['.', ',', '?', '!']
+	__punctuation_end_sentence = ['.', '?', '!']
+
+	def __init__(self, sample_file, dictionary_file):
+		"""
+		The sample file is used to gather statistical data about the
+		word, sentence and paragraph distributions in the text.
+
+		The dictionary file is used as a source of random words, such 
+		as the latin "lorem ipsum" words, to produce the output text.
+
+		Sample format:
+		Sentences separated by periods, question marks or exclamation
+		marks. Commas are counted. All other punctuation should
+		ideally be removed. Paragraphs are separated by empty lines.
+
+		Dictionary format:
+		Words separated by white space. Words are case-insensitive.
+		"""
+
+		self.__generate_dictionary(dictionary_file)
+		self.__generate_chains(sample_file)
+	
+	def __generate_dictionary(self, dictionary_file):
+		"""
+		Generate a dictionary of words, which are categorised by 
+		length, and which can be used in the output lipsum text.
+		"""
+
+		words = self.__load_contents(dictionary_file)
+		words = words.split()
+
+		dictionary = {}
+
+		for word in words:
+			length = len(word)
+			
+			if not dictionary.has_key(length):
+				dictionary[length] = []
+
+			dictionary[length] += [word]
+
+		# TODO:
+		# Raise error if there are no words in the
+		# dictionary
+
+		self.__dictionary = dictionary
+	
+	def __generate_chains(self, sample_file):
+		"""
+		Generate a dictionary of "chains", which are used to 
+		randomly create parts of a sentence.
+		Also, calculate the mean and standard deviation of
+		sentence lengths.
+		"""
+
+		words = self.__load_contents(sample_file)
+
+		sentence_split = ''
+		for delimiter in self.__punctuation_end_sentence:
+			sentence_split += '\\' + delimiter
+		sentence_split = '[' + sentence_split + ']'
+		sentences = re.split(sentence_split, words)
+
+		paragraphs = words.split('\n\n')
+		words = words.split()
+
+		previous = (0,0)
+		chains = {}
+		end_sentences = []
+
+		for word in words:
+			if not chains.has_key(previous):
+				chains[previous] = []
+
+			if self.__punctuation_end_word.count(word[-1:]):
+				end_word = word[-1:]
+				word.rstrip(end_word)
+			else:
+				end_word = ''
+				
+			length = len(word) 
+			chains[previous] += [(length, end_word)]
+			previous = (previous[1], length)
+
+			if self.__punctuation_end_sentence.count(end_word):
+				end_sentences += [previous]
+		
+		self.__chains = chains
+		self.__end_sentences = end_sentences
+
+		mu = 0
+		sigma = 0
+
+		for sentence in sentences:
+			words = len(sentence.split())
+			sigma += words**2
+			mu += words
+
+		mu /= len(sentences)
+		sigma /= len(sentences)
+		sigma -= mu**2
+		sigma = math.sqrt(sigma)
+
+		self.__sentence_mu = mu
+		self.__sentence_sigma = sigma
+
+		mu = 0
+		sigma = 0
+
+		for paragraph in paragraphs:
+			sentences = len(re.split(sentence_split, paragraph))
+			sigma += sentences**2
+			mu += sentences
+
+		mu /= len(paragraphs)
+		sigma /= len(paragraphs)
+		sigma -= mu**2
+		sigma = math.sqrt(sigma)
+
+		self.__paragraph_mu = mu
+		self.__paragraph_sigma = sigma
+	
+	def generate_sentence(self, start_with_lorem=False):
+		"""
+		generate_sentence() -> str
+
+		Generates a single sentence, of variable length.
+		"""
+		if start_with_lorem:
+			sentence = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet,', 'consecitur']
+			previous = (4, 10)
+		else:
+			sentence = []
+			previous = ()
+
+		mu = self.__sentence_mu
+		sigma = self.__sentence_sigma
+		chains = self.__chains
+		end_sentences = self.__end_sentences
+		dictionary = self.__dictionary
+
+		max_length = int(round(random.normalvariate(mu, sigma)))
+
+		for i in range(len(sentence), max(10, max_length)):
+			if chains.has_key(previous):
+				chain = random.choice(chains[previous])
+				length = chain[0]
+				word = random.choice(self.__choose_closest(dictionary, length))
+				word = word.lower()
+
+				end_word = chain[1]
+				previous = (previous[1], length)
+
+				sentence += [word + end_word]
+
+				if self.__punctuation_end_sentence.count(end_word):
+					break
+			else:
+				previous = random.choice(end_sentences)
+
+		sentence[0] = sentence[0].capitalize()
+
+		if not self.__punctuation_end_sentence.count(end_word):
+			if self.__punctuation_end_word.count(end_word):
+				sentence[-1] = sentence[-1].rstrip(end_word) + '.'
+			else:
+				sentence[-1] = sentence[-1] + '.'
+
+		sentence = string.join(sentence)
+		return sentence
+
+	def generate_paragraph(self, start_with_lorem=False):
+		"""
+		generate_paragraph() -> str
+
+		Generates a single paragraph, consisting of a variable number
+		of sentences of variable length.
+		"""
+		if start_with_lorem:
+			paragraph = [self.generate_sentence(start_with_lorem=True)]
+		else:
+			paragraph = []
+
+		mu = self.__paragraph_mu
+		sigma = self.__paragraph_sigma
+		max_length = int(round(random.normalvariate(mu, sigma)))
+
+		for i in range(len(paragraph), max(max_length, 1)):
+			paragraph += [self.generate_sentence()]
+
+		paragraph = string.join(paragraph)
+		return paragraph
+	
+	def __load_contents(self, file_name):
+		"""
+		Open a file and return its contents.
+		"""
+		file = open(file_name, 'r')
+		contents = file.read()
+		file.close()
+		return contents
+
+	def __choose_closest(self, dictionary, target_key):
+		"""
+		For dictionaries with numerical keys, choose the key closest to the
+		given number.
+		"""
+
+		if (isinstance(dictionary, dict) and isinstance(target_key, int)):
+			above = False
+			below = False
+
+			for key in dictionary.iterkeys():
+				if isinstance(key, int):
+					above = key
+					if target_key < key:
+						if below == False:
+							below = above
+						break
+					below = key
+
+			if abs(above - target_key) < abs(below - target_key):
+				return dictionary[above]
+			else:
+				return dictionary[below]
---
+++ main.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0') 
+# TODO: Cross-check the widgets and options I use with the documentation
+# to see which version I really require
+import gtk
+
+import lipsum
+import string
+
+class Main:
+	def __init__(self):
+		self.__initialize_window()
+	
+	def __initialize_window(self):
+		# Output area
+		self.__textbuffer_output = gtk.TextBuffer()
+
+		self.__textview_output = gtk.TextView(buffer=self.__textbuffer_output)
+		self.__textview_output.set_wrap_mode(gtk.WRAP_WORD_CHAR)
+		self.__textview_output.set_editable(False)
+
+		self.__scrolledwindow_output = gtk.ScrolledWindow()
+		self.__scrolledwindow_output.set_shadow_type(gtk.SHADOW_IN)
+		self.__scrolledwindow_output.add(self.__textview_output)
+		self.__scrolledwindow_output.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+		self.__alignment_output = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=1.0, yscale=1.0)
+		self.__alignment_output.add(self.__scrolledwindow_output)
+		self.__alignment_output.set_padding(0, 0, 12, 0)
+
+		self.__frame_output = gtk.Frame(label='<b>Output</b>')
+		self.__frame_output.get_label_widget().set_use_markup(True)
+		self.__frame_output.add(self.__alignment_output)
+		self.__frame_output.set_shadow_type(gtk.SHADOW_NONE)
+
+		# Options area
+		self.__spinbutton_quantity = gtk.SpinButton()
+		self.__spinbutton_quantity.set_range(1, 999)
+		self.__spinbutton_quantity.set_increments(1, 10)
+		self.__spinbutton_quantity.set_width_chars(4)
+		self.__spinbutton_quantity.set_value(5)
+
+		self.__radiobutton_quantity_paragraphs = gtk.RadioButton(label='Paragraphs')
+		self.__radiobutton_quantity_sentences = gtk.RadioButton(label='Sentences', group=self.__radiobutton_quantity_paragraphs)
+
+		self.__vbox_quantity_radiobuttons = gtk.VBox()
+		self.__vbox_quantity_radiobuttons.add(self.__radiobutton_quantity_paragraphs)
+		self.__vbox_quantity_radiobuttons.set_child_packing(self.__radiobutton_quantity_paragraphs, False, True, 0, gtk.PACK_START)
+		self.__vbox_quantity_radiobuttons.add(self.__radiobutton_quantity_sentences)
+		self.__vbox_quantity_radiobuttons.set_child_packing(self.__radiobutton_quantity_sentences, False, True, 0, gtk.PACK_START)
+
+		self.__hbox_quantity = gtk.HBox(spacing=6)
+		self.__hbox_quantity.add(self.__spinbutton_quantity)
+		self.__hbox_quantity.set_child_packing(self.__spinbutton_quantity, False, True, 0, gtk.PACK_START)
+		self.__hbox_quantity.add(self.__vbox_quantity_radiobuttons)
+		self.__hbox_quantity.set_child_packing(self.__vbox_quantity_radiobuttons, False, True, 0, gtk.PACK_START)
+
+		self.__alignment_quantity = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=1.0, yscale=1.0)
+		self.__alignment_quantity.add(self.__hbox_quantity)
+		self.__alignment_quantity.set_padding(0, 0, 12, 0)
+
+		self.__frame_quantity = gtk.Frame(label='<b>Quantity</b>')
+		self.__frame_quantity.get_label_widget().set_use_markup(True)
+		self.__frame_quantity.add(self.__alignment_quantity)
+		self.__frame_quantity.set_shadow_type(gtk.SHADOW_NONE)
+		
+		self.__checkbutton_startwithlorem = gtk.CheckButton(label='Start with "Lorem ipsum..."')
+
+		self.__alignment_options = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=1.0, yscale=1.0)
+		self.__alignment_options.add(self.__checkbutton_startwithlorem)
+		self.__alignment_options.set_padding(0, 0, 12, 0)
+
+		self.__frame_options = gtk.Frame(label='<b>Options</b>')
+		self.__frame_options.get_label_widget().set_use_markup(True)
+		self.__frame_options.add(self.__alignment_options)
+		self.__frame_options.set_shadow_type(gtk.SHADOW_NONE)
+		#self.__frame_options.set_child_packing(self.__checkbutton_startwithlorem, False, True, 0, gtk.PACK_START)
+
+		self.__hbox_options = gtk.HBox(spacing=6)
+		self.__hbox_options.add(self.__frame_options)
+		self.__hbox_options.set_child_packing(self.__frame_options, False, True, 0, gtk.PACK_END)
+		self.__hbox_options.add(self.__frame_quantity)
+		self.__hbox_options.set_child_packing(self.__frame_quantity, False, True, 0, gtk.PACK_END)
+
+		# Actions area
+		self.__button_copy = gtk.Button(stock=gtk.STOCK_COPY)
+		self.__button_copy.connect('clicked', self.__copy_output)
+
+		self.__button_generate = gtk.Button(label='Generate text')
+		self.__button_generate.connect('clicked', self.__generate_output)
+
+		self.__hbox_actions = gtk.HBox(spacing=6)
+		self.__hbox_actions.add(self.__button_generate)
+		self.__hbox_actions.set_child_packing(self.__button_generate, False, True, 0, gtk.PACK_END)
+		self.__hbox_actions.add(self.__button_copy)
+		self.__hbox_actions.set_child_packing(self.__button_copy, False, True, 0, gtk.PACK_END)
+
+		# Main window / vbox
+
+		self.__vbox_main = gtk.VBox(spacing=12)
+		self.__vbox_main.add(self.__frame_output)
+		self.__vbox_main.set_child_packing(self.__frame_output, True, True, 0, gtk.PACK_START)
+		self.__vbox_main.add(self.__hbox_options)
+		self.__vbox_main.set_child_packing(self.__hbox_options, False, True, 0, gtk.PACK_START)
+		self.__vbox_main.add(self.__hbox_actions)
+		self.__vbox_main.set_child_packing(self.__hbox_actions, False, True, 0, gtk.PACK_START)
+		self.__vbox_main.set_border_width(12)
+
+		self.__window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		self.__window.set_title("Lorem ipsum generator")
+		self.__window.connect('destroy', self.__destroy)
+		self.__window.add(self.__vbox_main)
+		self.__window.show_all()
+
+		self.__clipboard = gtk.Clipboard()
+	
+	def main(self):
+		gtk.main()
+
+	def __destroy(self, widget, data=None):
+		gtk.main_quit()
+	
+	def __copy_output(self, widget, data=None):
+		self.__clipboard.set_text(
+				self.__textbuffer_output.get_text(
+					self.__textbuffer_output.get_start_iter(),
+					self.__textbuffer_output.get_end_iter()
+					)
+				)
+
+	def __generate_output(self, widget, data=None):
+		try:
+			sample = 'sample.txt'
+			dictionary = 'dictionary.txt'
+			generator = lipsum.generator(sample, dictionary)
+		except IOError, (errno, strerror):
+			error = gtk.MessageDialog(parent=self.__window, type=gtk.MESSAGE_ERROR, message_format='Could not locate one of the dictionary or sample files, "%s" or "%s", respectively.' % (dictionary, sample), buttons=gtk.BUTTONS_OK)
+			def close_error(widget, data=None):
+				widget.destroy()
+			error.connect('response', close_error)
+			error.show()
+			return
+
+		quantity = self.__spinbutton_quantity.get_value_as_int()
+
+		text = []
+
+		for i in range(len(text), quantity):
+			if self.__radiobutton_quantity_paragraphs.get_active():
+				text += [generator.generate_paragraph()]
+			elif self.__radiobutton_quantity_sentences.get_active():
+				text += [generator.generate_sentence()]
+		
+		if self.__radiobutton_quantity_paragraphs.get_active():
+			if self.__checkbutton_startwithlorem.get_active():
+				text[0] = generator.generate_paragraph(start_with_lorem=True)
+			text = string.join(text, '\n\n')
+		elif self.__radiobutton_quantity_sentences.get_active():
+			if self.__checkbutton_startwithlorem.get_active():
+				text[0] = generator.generate_sentence(start_with_lorem=True)
+			text = string.join(text, ' ')
+
+		self.__textbuffer_output.set_text(text)
+
+if __name__ == "__main__":
+	main = Main()
+	main.main()
