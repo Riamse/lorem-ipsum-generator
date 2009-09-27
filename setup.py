3398175784cce9078585223035a00046144bf943
jhales.perth <jhales.perth@ea057288-b53e-0410-a93f-1b578e867aa5>
2009-09-27 05:47:07 +0000

    Hopefully fixed packaging so that the lipsum.xml file can be found when lipsum is run.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 
 setup(
 		name = 'lorem-ipsum-generator',
-		version = 'trunk'
+		version = 'trunk',
 		description = 'Generates random lorem ipsum text',
 		long_description = 'Lorem Ipsum Generator provides a GTK+ graphical user interface, and a Python module, that generates random "lorem ipsum" text. The Lorem Ipsum Generator can produce a given quantity of paragraphs or sentences of "lorem ipsum" text.',
 		author = 'James Hales',
@@ -14,7 +14,12 @@ setup(
 		license = 'BSD License',
 		url = 'http://code.google.com/p/lorem-ipsum-generator/',
 		py_modules = ['lipsum'],
-        scripts = ['lipsum', 'lipsum_postinstall.py'],
-		data_files = [('share/lipsum', ['sample.txt', 'dictionary.txt']), ('share/doc/lorem-ipsum-generator', ['README', 'INSTALL', 'COPYING']), ('share/applications', ['lipsum.desktop'])]
+        scripts = ['lipsum', 'src/lipsum_postinstall.py'],
+		data_files = [('share/doc/lorem-ipsum-generator', ['README', 'INSTALL', 'COPYING']), ('share/applications', ['data/lipsum.desktop'])],
+        packages = find_packages('src'),
+        package_dir = {'' : 'src'},
+        package_data = {
+            'lipsum': ['data/lipsum.xml'],
+            }
 		)
 
